a296587b420f3977f6c6fe0981899d9c1e619fb2
vsparrow <khan@vsparrow.com>
2018-05-14 10:05:54 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+  state = {text: ""}
+
+  handleChange = (e)=>{
+    this.setState({text: e.target.value},()=>{console.log("inner state");console.log(this.state)})}
+  handleSubmit = (e)=>{
+    e.preventDefault();
+    // console.log("inside submit");
+    // console.log(this.props.store);
+    // console.log(this.props.store.dispatch);
+    this.props.store.dispatch({type: 'ADD_BAND', band: this.state.text})
+    this.setState({text: ''})
+  }
+//Note: To have access to the store's dispatch method, we pass through the entire store as a prop.
+
   render() {
     return (
       <div>
         bands component
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label htmlFor="BandInput">BandInput</label>
+            <input type="text" value={this.state.text}  onChange={this.handleChange}/>
+          </p>
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
 };
 
 export default BandInput;
+
+// The component should have a form
+//   text input
+//   submit button.
+// update the component state
+//   each time the user types a new character in the input.
+//   only update the stores state
+//     when the user hits the submit button.
+//
+// Note: To have access to the stores dispatch method, we pass through the entire store as a prop.
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -8,3 +8,11 @@ export default function manageBand(state = {
       return state;
   }
 };
+
+//
+// You will also need to fill in the manageBand reducer located under ./src/reducers/manageBand.js.
+// It will need to respond to an action of type 'ADD_BAND'
+//   and correspondingly concatenate a new band each time the action is dispatched.
+
+
+//above was already there.
